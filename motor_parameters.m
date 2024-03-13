4167b2ba11d465486623490d0de9881e74e6a008
Jayden <jaydeng832@gmail.com>
2024-03-12 18:14:50 -0700

    updated model

--- motor_parameters.m
+++ motor_parameters.m
@@ -11,3 +11,22 @@ J = 7.89E-2;
 % Km = 1.146E-2;
 % b = 1.64E-6;
 % J = 0.8E-4;
+
+s = tf('s');
+P_motor = Km/(s*((J*s+b)*(L*s+R)+Km^2));
+zpk(P_motor)
+
+Ts = 0.001;
+dP_motor = c2d(P_motor, Ts, 'zoh');
+zpk(dP_motor)
+
+dP_motor = minreal(dP_motor,0.001);
+zpk(dP_motor)
+
+sys_cl = feedback(dP_motor,1);
+[x1,t] = step(sys_cl,10);
+stairs(t,x1)
+xlabel('Time (seconds)')
+ylabel('Position (radians)')
+title('Stairstep Response: Original')
+grid
